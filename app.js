68a2d50ace879c1ef43169033dd365b9d557f605
Karim <karim.ayoub.job@gmail.com>
2025-06-16 17:09:55 +0200

    Add /users

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 // Mise en place code express API
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get('/',(req,res)=>{
-    res.send("Hello world");
+const users = [
+  { id: 1, name: "Rimk" },
+  { id: 2, name: "Jouli" },
+  { id: 3, name: "Imen" },
+];
+
+app.get("/", (req, res) => {
+  res.send("Hello world");
 });
 
-app.listen(port,()=>{
-    console.log(`Server is running on port ${port}`);
+app.get("/users", (req, res) => {
+  res.json(users);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
