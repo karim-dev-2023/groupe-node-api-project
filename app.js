79a6b5cc8eaa0131b5d30aba1fcbe9e2c7814f64
midou-bit <midou@DESKTOP-GEB5SA4.localdomain>
2025-06-17 14:13:09 +0200

    requete_post

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
-// Mise en place code express API
 const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/',(req,res)=>{
-    res.send("Hello world");
+app.use(express.json());
+
+const usersRoutes = require('./routes/users');
+app.use('/users', usersRoutes);
+
+app.get('/', (req, res) => {
+    res.send("siuuuu");
 });
 
-app.listen(port,()=>{
-    console.log(`Server is running on port ${port}`);
+app.listen(port, () => {
+    console.log(`le serveur est lance sur le port ${port}`);
 });
