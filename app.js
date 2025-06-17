162995df51ec58d0e8ce106e676c9c7a8bd22084
Karim <karim.ayoub.job@gmail.com>
2025-06-17 13:47:41 +0200

    Add handling error route

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 // Mise en place code express API
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get('/',(req,res)=>{
-    res.send("Hello world");
+app.get("/", (req, res) => {
+  res.send("Hello world");
 });
 
-app.listen(port,()=>{
-    console.log(`Server is running on port ${port}`);
+// On gère la gestion d'erreur lorsque la route n'existe pas
+app.use((req, res) => {
+  res.status(404).send("Cette page n'existe pas ");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
